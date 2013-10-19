c85319ef52a681734a67e9567159ad24be6c9693
barelyknown <sean@buytruckload.com>
2013-10-19 10:55:23 -0500

    rebuild binstubs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 require 'bundler/capistrano'
- 
+
 # This capistrano deployment recipe is made to work with the optional
 # StackScript provided to all Rails Rumble teams in their Linode dashboard.
 #
@@ -24,32 +24,32 @@ require 'bundler/capistrano'
 #
 # Note: When deploying, you'll be asked to enter your server's root
 # password. To configure password-less deployments, see below.
- 
+
 #############################################
 ##                                         ##
 ##              Configuration              ##
 ##                                         ##
 #############################################
- 
+
 GITHUB_REPOSITORY_NAME = 'r13-team-474'
 LINODE_SERVER_HOSTNAME = '212.71.239.151'
- 
+
 #############################################
 #############################################
- 
+
 # General Options
- 
+
 set :bundle_flags,               "--deployment"
- 
+
 set :application,                "railsrumble"
 set :deploy_to,                  "/var/www/apps/railsrumble"
 set :normalize_asset_timestamps, false
 set :rails_env,                  "production"
- 
+
 set :user,                       "root"
 set :runner,                     "www-data"
 set :admin_runner,               "www-data"
- 
+
 # Password-less Deploys (Optional)
 #
 # 1. Locate your local public SSH key file. (Usually ~/.ssh/id_rsa.pub)
@@ -60,25 +60,25 @@ set :admin_runner,               "www-data"
 # 3. Uncomment the below ssh_options[:keys] line in this file.
 #
 # ssh_options[:keys] = ["~/.ssh/id_rsa"]
- 
+
 # SCM Options
 set :scm,        :git
 set :repository, "git@github.com:railsrumble/#{GITHUB_REPOSITORY_NAME}.git"
 set :branch,     "master"
- 
+
 # Roles
 role :app, LINODE_SERVER_HOSTNAME
 role :db,  LINODE_SERVER_HOSTNAME, :primary => true
- 
+
 # Add Configuration Files & Compile Assets
 after 'deploy:update_code' do
   # Setup Configuration
   run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
- 
+
   # Compile Assets
   run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
 end
- 
+
 # Restart Passenger
 deploy.task :restart, :roles => :app do
   # Fix Permissions
@@ -86,7 +86,7 @@ deploy.task :restart, :roles => :app do
   sudo "chown -R www-data:www-data #{latest_release}"
   sudo "chown -R www-data:www-data #{shared_path}/bundle"
   sudo "chown -R www-data:www-data #{shared_path}/log"
- 
+
   # Restart Application
   run "touch #{current_path}/tmp/restart.txt"
 end
